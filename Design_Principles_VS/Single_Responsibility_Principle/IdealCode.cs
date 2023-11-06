4ddd349e385deaf29efbfdb4e0824cd68dff8f57
Erkan Ozvatan <95587733+nominal88eo@users.noreply.github.com>
2023-11-06 21:02:03 +0200

    Add files via upload

---
+++ Design_Principles_VS/Single_Responsibility_Principle/IdealCode.cs
@@ -0,0 +1,29 @@
+﻿using Single_Responsibility_Principle;
+
+namespace Single_Responsibility_Principle_IdealCode;
+class Database
+{
+    public void Connect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public void Disconnect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+    }
+    public string State { get; set; }
+}
+
+class PersonService
+{
+    public List<Person> GetPersons()
+    {
+        return new() {
+            new(){ Name = "Hilmi", Surname = "Celayir" },
+            new(){ Name = "Mustafa", Surname = "Yıldız" },
+            new(){ Name = "Cafer", Surname = "Muiddinoğlu" }
+        };
+    }
+}
